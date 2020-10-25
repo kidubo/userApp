ba1bcc478e4c16ca0cb67c0aa4f13b84c6c3096d
kidubo <innocentleonard007@gmail.com>
2020-10-25 03:18:23 +0300

    sendgridapi

--- src/emails/account.js
+++ src/emails/account.js
@@ -2,35 +2,31 @@ const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-
 const sendWelcomeEmail = (email, name) => {
 
-    sgMail.send({
+    sgMail
+        .send({
             to: email,
             from: 'innocentleonard0007@gmail.com',
             subject: 'Thanks for joining in!',
             text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
-        }).then(() => {
-            console.log('Email sent')
         })
-        .catch((error) => {
-            console.error(error)
-        })
+        .then(() => { console.log('Email sent') })
+        .catch((error) => { console.error(error) })
 
 }
 
+
 const sendCancelationEmail = (email, name) => {
-    sgMail.send({
+    sgMail
+        .send({
             to: email,
             from: 'innocentleonard0007@gmail.com',
             subject: 'Sorry  to see you go!',
             text: `Goodbye, ${name}. I hope to see you back sometime soon.`
-        }).then(() => {
-            console.log('Email sent')
         })
-        .catch((error) => {
-            console.error(error)
-        })
+        .then(() => { console.log('Email sent') })
+        .catch((error) => { console.error(error) })
 }
 
 module.exports = {
